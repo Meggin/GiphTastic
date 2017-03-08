409692b24d4b15a1496706a69277e238e3b6b87b
mkearney@google.com <mkearney@google.com>
2017-03-07 16:02:07 -0800

    a better approach in pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,20 @@
 // Todo: create an on click event for submit button.
-// When this event fires, call the giphy sticker API to retrieve giph data
-// based on the user input, i.e. chosen giph topic.
 
-// Todo: append the user input to an array of strings, called topics.
+// Todo: when this event fires, append the user input to an array of strings, call topics.
 
 // Todo: create function to create buttons for each item in the array.
 // These buttons live in buttons-container.
 // Need to work out if we should have a separate function for adding a single item to array
 // and creating one new button at a time, in addition to creating full list of buttons from the array.
 
-// Todo: need to call the Giphy API with search parameters based on user input.
+// Todo: create an event listener for when a user clicks on a topic button.
+// When this event fires, need to remove the existing giphs displayed,
+// showing giphs for selected topic.
+
+// Todo: when user clicks submit, or when user clicks on button, it is triggering the call to the Giphy API.
+
+// Todo: create function to call the Giphy API with search parameters based on the item clicked on,
+// be it either the Submit button with user input values, or the button clicked on with a button title.
 // Should return 10 gifs that match the search parameter.
 // We need a static image, the mp4 for animation, the rating, and possibly other details like caption.
 
@@ -24,17 +29,6 @@
 // Todo: create function to stop giph animation when user clicks a second time on a giph.
 // Again, using mp4 to stop animations.
 // And again, will use video API.
-
-// Todo: create an event listener for when a user clicks on a topic button.
-// When this event fires, need to remove the existing giphs displayed,
-// showing giphs for selected topic.
-
-
-// Todo: need to work out how we store the data that comes back from Giphy for each topic selected.
-// Todo: one tricky bit will be connecting a button to the data item.
-// Todo: So for example, let's say we have dog button. When you click on dog button,
-// dog giphs should display. Could relate them by a data attribute which is the same
-// as the Giphy API search topic.
 
 // Todo: I'm fairly sure that we don't want to hold up any content from appearing on the page,
 // if available, so we will want to be careful about calling for data outside document.ready?
